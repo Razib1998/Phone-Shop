49682d560413cdf0b535b2b862718a3db76358eb
Md   Razib Sarker <mdrajibsorkar965@gmail.com>
2023-11-02 21:46:23 +0100

    Basic Set up completed

---
+++ src/Layout/MainLayout.jsx
@@ -0,0 +1,13 @@
+import { Outlet } from "react-router-dom";
+
+
+const MainLayout = () => {
+    return (
+        <div>
+            <h2>This is Navbar</h2>
+            <Outlet></Outlet>
+        </div>
+    );
+};
+
+export default MainLayout;
